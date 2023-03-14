717cc9434728136b9a352a72010168bcfb1dfd6d
Aripp7 <ahmadarif9085@gmail.com>
2023-03-14 15:15:46 +0700

    new

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,10 +15,10 @@ const Home = () => {
                     Ahmad Arif
                 </h1>
                 <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
-                    I'm a Frontend Web Developer.
+                    I'm a Web Developer.
                 </h2>
                 <p className='text-[#8892b0] py-4 max-w-[700px]'>
-                    I'm a frontend web developer who specializes in building (and sometimes designing) a great digital experience.
+                    I'm a web developer who specializes in building (and sometimes designing) a great digital experience.
                     Right now, I'm focusing on build responsive web applications.
                 </p>
                 <div>
