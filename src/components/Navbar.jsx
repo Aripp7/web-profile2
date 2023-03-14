c07a58b07d58de20b86473443b4336c7618bca8b
Aripp7 <ahmadarif9085@gmail.com>
2023-03-14 15:09:53 +0700

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -124,7 +124,7 @@ const Navbar = () => {
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
                         <a
                             className='flex justify-between items-center w-full text-gray-300'
-                            href='Ahmad Arif CV.pdf'
+                            href='CV Ahmad Arif New.pdf'
                         >
                             Resume <BsFillPersonLinesFill size={30} />
                         </a>
